09a51cb0f15fe81d5ae8fb00bea22bd4035d4602
Witcher5566 <github77881@gmail.com>
2025-06-26 12:52:15 +0530

    Update info.py

--- info.py
+++ info.py
@@ -57,7 +57,7 @@ UPDATE_CHANNEL_LNK = environ.get('UPDATE_CHANNEL_LNK', 'https://t.me/Patrick_Bot
 AUTH_CHANNEL = int(environ.get('AUTH_CHANNEL', '-1002343174334')) 
 AUTH_REQ_CHANNEL = int(environ.get('AUTH_REQ_CHANNEL', '-1002343174334'))
 
-IS_VERIFY = is_enabled('IS_VERIFY', False)
+IS_VERIFY = is_enabled('IS_VERIFY', True)
 LOG_VR_CHANNEL = int(environ.get('LOG_VR_CHANNEL', '-1002149281691')) #Verification Channel Id 
 LOG_API_CHANNEL = int(environ.get('LOG_API_CHANNEL', '-1002149281691')) #If Anyone Set Your Bot In Any Group And Set Shortner In That Group Then In This Channel The All Details Come
 VERIFY_IMG = environ.get("VERIFY_IMG", "https://graph.org/file/4afad3acc7e35ab6b1a35-4262db63d9f78220b2.jpg")
